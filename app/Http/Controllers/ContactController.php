6a93d956c5a3bae6f2ef349f22ce0e65fb60d1ea
Kalungipatricia <kalungipatricia66@gmail.com>
2017-08-03 10:41:40 +0300

    add contact form to the project

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Requests\ContactFormRequest;
 
 class ContactController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('welcome');
+    }
+
+    public function store(ContactFormRequest $request)
+    {
+        return \Redirect::route('welcome')
+            ->with('message', 'Thanks for contacting us!');
+
+
+    }
+
 }
